f2ba9b2d465ec4f3d616a3869774558d2b04b228
BikramdeepSingh <biksaini20@gmail.com>
2022-02-10 18:47:47 +0530

    adding chart to xlsx

--- automate_spreadsheet.py
+++ automate_spreadsheet.py
@@ -1,10 +1,11 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 
-def process_spreadsheet(filename):
+def process_spreadsheet(filename,column_heading):
     wb = xl.load_workbook(filename)
     sheet = wb['Sheet1']
-
+    sheet.cell(1,4).value=column_heading
     #to get the coordinate of a cell=sheet.cell(x,y) or sheet[a1] 
     #and cell.value to fetch the value in a cell
 
@@ -14,7 +15,17 @@ def process_spreadsheet(filename):
         corrected_price_cell = sheet.cell(row,4)
         corrected_price_cell.value = corrected_price
 
-    wb.save('transactions_test.xlsx')
+    values= Reference(sheet, 
+               min_row=2,
+               max_row=sheet.max_row,
+               min_col=4,
+               max_col=4 )
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'A13')
+
+    wb.save(filename)
 
 
-process_spreedsheet('transactions.xlsx')
+process_spreadsheet('transactions.xlsx','updated_price')
